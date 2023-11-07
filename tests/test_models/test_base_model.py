97028dceee861433c1b7b74efe88fd2b7c67c29e
masmiyen <masmiyen@gmail.com>
2023-11-07 19:07:38 +0100

    unit test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
 import unittest
+import datetime
+from uuid import UUID
+import json
+import os
 
 class TestBaseModel(unittest.TestCase):
 
