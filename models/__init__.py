3bff4275b0b605f6ca4298e253e33f34adaaf911
masmiyen <masmiyen@gmail.com>
2023-11-09 23:25:37 +0100

    file storage2

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
+
 """An instance of the FileStorage class is created in this module"""
 from models.engine.file_storage import FileStorage
 
 
 storage = FileStorage()
 storage.reload()
-=======
-
-
-from models.engine import file_storage
-
-storage = file_storage.FileStorage()
-storage.reload()
->>>>>>> 76b5363d195b5a9bf9fd9a7dc4640b2333668d2f
