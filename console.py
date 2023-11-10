20a36d5d26487cd14fdb3a659773a325a2190412
masmiyen <masmiyen@gmail.com>
2023-11-10 19:42:48 +0100

    console

--- console.py
+++ console.py
@@ -6,9 +6,22 @@ from models.base_model import BaseModel
 from models import storage
 import json
 
+
 class HBNBCommand(cmd.Cmd):
-    """HBNBCommand class"""
-    prompt = "(hbnb) "
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """An empty line + ENTER shouldn’t execute anything"""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
