03822cb53528c4cc8691ca0fa717b64055105505
0ussamabernou <oussama.bernou@outlook.com>
2023-11-11 23:59:05 +0100

    fix AttributeError: 'str' object has no attribute 'isoformat'. Did you mean: 'format'?

--- models/base_model.py
+++ models/base_model.py
@@ -52,7 +52,7 @@ class BaseModel:
         returns a dictionary containing all keys/values
         of __dict__ of the instance
         """
-        this_dict = self.__dict__
+        this_dict = self.__dict__.copy()
         this_dict["__class__"] = self.__class__.__name__
         this_dict["updated_at"] = self.updated_at.isoformat()
         this_dict["created_at"] = self.created_at.isoformat()
