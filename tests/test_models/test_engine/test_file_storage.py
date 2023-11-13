15f44768633c9d10c4ceb41ab5704ce6c7557630
0ussamabernou <oussama.bernou@outlook.com>
2023-11-13 17:11:32 +0100

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,76 +2,51 @@
 
 """Unittest for file_storage.py"""
 
-import os
 import unittest
 from models.engine import file_storage
 import json
 
 
 class TestFileStorage(unittest.TestCase):
-    def setUp(self):
+    def test_file_storage(self):
         """
-        Set up for the tests.
+        Test the file storage class
         """
+        store = file_storage.FileStorage()
+        self.assertIsInstance(store, file_storage.FileStorage)
+        # self.assertEqual(store.__file_path, "file.json")
+        # self.assertIsInstance(store.__objects, dict)
+        # self.assertEqual(store.__objects, {})
+        self.assertTrue(hasattr(store, "all"))
+        self.assertTrue(hasattr(store, "new"))
+        self.assertTrue(hasattr(store, "save"))
+        self.assertTrue(hasattr(store, "reload"))
 
-        try:
-            os.rename("file.json", "tmp.json")
-        except IOError:
-            pass
-        self.storage = file_storage.FileStorage()
-        self.storage.__file_path = "test.json"
-
-    def tearDown(self):
-        """Tears down test methods"""
-        try:
-            os.remove("file.json")
-            os.rename("tmp.json", "file.json")
-        except IOError:
-            pass
-
-    def test_all(self):
-        """
-        Test that all returns the dictionary __objects
-        """
-        self.assertEqual(type(self.storage.all()), dict)
-
-    def test_new(self):
-        """
-        Test that new adds an object to __objects
-        """
         try:
             with open("test.json") as f:
                 obj = json.load(f)
         except (FileNotFoundError, FileExistsError):
             return
-        self.storage.new(obj)
-        self.assertIn(obj, self.storage.all().values())
+        store.new(obj)
+        self.assertIn(obj, store.all().values())
 
-    def test_save(self):
-        """
-        Test that save properly serializes __objects to the JSON file
-        """
         try:
             with open("test.json") as f:
                 obj = json.load(f)
         except (FileNotFoundError, FileExistsError):
             return
-        self.storage.save()
+        store.save()
         with open("test.json") as f:
             obj2 = json.load(f)
         self.assertEqual(obj, obj2)
 
-    def test_reload(self):
-        """
-        Test that reload properly deserializes the JSON file to __objects
-        """
         try:
             with open("test.json") as f:
                 obj = json.load(f)
         except (FileNotFoundError, FileExistsError):
             return
-        self.storage.reload()
-        self.assertEqual(obj, self.storage.all())
+        store.reload()
+        self.assertEqual(obj, store.all())
 
 
 if __name__ == "__main__":
